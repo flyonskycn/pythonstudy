6e15041f17acc5144979a1c0786f5f3177296204
luowengang <2628387671@qq.com>
2019-02-15 18:31:23 +0800

    python基本语法使用

--- pythonstudy/hello.py
+++ pythonstudy/hello.py
@@ -4,4 +4,16 @@ Created on 2019年2月15日
 @author: luowg
 '''
 
-print("Hello")
+print('hello')
+
+num1 = input('请输入第一个数字:')
+num2 = input('请输入第二个数字:')
+
+sum1 = float(num1) + float(num2)
+
+print('数字 {0} 和 {1} 相加结果为: {2}'.format(num1, num2, sum1))
+
+num3 = float(input('请输入一个数字:'))
+
+print('数字{0}平方结果:{1}'.format(num3,num3**2))
+print('数字{0}平方根结果:{1:0.3f}'.format(num3,num3**0.5))
